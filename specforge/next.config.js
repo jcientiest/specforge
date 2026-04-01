9a118841308a9d16ea981483a0aedf95fc135e75
jcientiest <64419202+jcientiest@users.noreply.github.com>
2026-04-01 15:18:08 +0530

    Update next.config.js

--- specforge/next.config.js
+++ specforge/next.config.js
@@ -1,6 +1,10 @@
+const repo = "specforge"; // MUST match your repo name
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   trailingSlash: true,
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
